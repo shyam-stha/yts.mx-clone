1f82b7af418a44339fcde912d1108a1560e97f52
Shyam Stha <sthask2019@gmail.com>
2022-12-24 15:18:11 +0545

    Fix : Add props value to the elements

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -5,18 +5,20 @@ const MovieCard = ({ movieItem, quality, background_image, title, year, rating, 
     return (
         <div className="card-wrapper">
             <div className='movie-card' style={{ backgroundImage: `url(${background_image})` }}>
-                <div className="quality">${quality}</div>
+                <div className="quality">{quality}</div>
                 <div className="card-content">
                     <img src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png" alt="star" />
-                    <div className="rating">${rating}/10</div>
-                    <div className="genres">${genres}</div>
+                    <div className="rating">{rating}/10</div>
+                    <div className="genres">{genres.map((genre,idx)=>{return <div key={idx}>{genre}</div>})}</div>
                     <button className="view-dtl">View Details</button>
                 </div>
             </div>
-            <h4 className="title">${title}fjldkjfl</h4>
-            <span className="year">${year}1205</span>
+            <h4 className="title">{title}</h4>
+            <span className="year">{year}</span>
         </div>
     )
 }
+
+
 
 export default MovieCard
