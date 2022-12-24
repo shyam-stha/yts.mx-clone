843885c24266a22d97b3e1693a2141d95d3c4340
Shyam Stha <sthask2019@gmail.com>
2022-12-24 15:19:10 +0545

    Feat : Add movie card component and render it with movies details props

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,19 +1,41 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchMovie } from '../../app/actions/fetchMovie'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovie } from "../../app/actions/fetchMovie";
+import MovieCard from "../movieCard/MovieCard";
+import "./movie-list.scss"
 
 const MovieList = () => {
-    const { isLoading, error, movie } = useSelector(state => state.movie)
+    const { isLoading, error, movie } = useSelector((state) => state.movie);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchMovie());
-    }, [])
+    }, []);
 
     return (
-        <div className='movie-list'>
+        <div className="movie-list">
+            <div className="list-container">
+                {!isLoading ? (
+                    movie.map((movieItem) => {
+                        return (
+                            <MovieCard
+                                key={movieItem.id}
+                                movieItem={movieItem}
+                                quality={movieItem.torrents[0].quality}
+                                background_image={movieItem.medium_cover_image}
+                                title={movieItem.title}
+                                year={movieItem.year}
+                                rating={movieItem.rating}
+                                genres={movieItem.genres}
+                            />
+                        );
+                    })
+                ) : (
+                    <h1>Loading ....</h1>
+                )}
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default MovieList
+export default MovieList;
