d5d4108c7097244935e83d92a1bea0e10b1f4e05
Shyam Stha <sthask2019@gmail.com>
2022-12-24 17:43:10 +0545

    Moved components from components directory to layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import MovieList from './layouts/movieList/MovieList'
+import MovieCard from './layouts/movieCard/MovieCard'
+import AppBar from './layouts/appbar/AppBar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div style={{backgroundColor : "black", height : "100vh"}}>
+      <AppBar />
+      {/* <MovieList /> */}
+      {/* <MovieCard /> */}
+    </div>
   )
 }
 
