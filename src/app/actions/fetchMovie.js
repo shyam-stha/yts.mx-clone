db65d43eadf7b6bba359224394d449d0f6bbc61b
Shyam Stha <sthask2019@gmail.com>
2022-12-25 11:34:50 +0545

    Create async thunk function to fetch upcomming movie list

--- src/app/actions/fetchMovie.js
+++ src/app/actions/fetchMovie.js
@@ -5,9 +5,23 @@ export const fetchMovie = createAsyncThunk(
   "movie/fetchMovie",
   async (thunkAPI) => {
     try {
-      const response = await axios.get("/");
+      const response = await axios.get("/list_movies.json");
       const data = response.data;
-      console.log(data.data.movies)
+      console.log(data.data.movies);
+      return data.data.movies;
+    } catch (error) {
+      throw thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const fetchUpcommingMovie = createAsyncThunk(
+  "movie/fetchUpcommingMovie",
+  async (thunkAPI) => {
+    try {
+      const response = await axios.get("/list_upcoming.json");
+      const data = response.data;
+      console.log(data.data.movies);
       return data.data.movies;
     } catch (error) {
       throw thunkAPI.rejectWithValue(error);
