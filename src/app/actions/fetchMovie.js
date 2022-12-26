c702d763110b63d93c60dedc17de8089f221895a
Shyam Stha <sthask2019@gmail.com>
2022-12-26 13:42:40 +0545

    Fix : Fix async function that fetch movie detatils

--- src/app/actions/fetchMovie.js
+++ src/app/actions/fetchMovie.js
@@ -5,7 +5,7 @@ export const fetchMovie = createAsyncThunk(
   "movie/fetchMovie",
   async (thunkAPI) => {
     try {
-      const response = await axios.get("/list_movies.json");
+      const response = await axios.get("/list_movies.json?page=2");
       const data = response.data;
       return data.data.movies;
     } catch (error) {
@@ -34,6 +34,19 @@ export const fetchSuggestedMovie = createAsyncThunk(
       const response = await axios.get("/movie_suggestions.json?movie_id=10");
       const data = response.data;
       return data.data.movies;
+    } catch (error) {
+      throw thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const fetchSelectedMovie = createAsyncThunk(
+  "movie/fetchSelectedMovie",
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.get(`/movie_details.json?movie_id=${id}&with_images=true&with_cast=true`);
+      const data = response.data;
+      return data.data.movie;
     } catch (error) {
       throw thunkAPI.rejectWithValue(error);
     }
